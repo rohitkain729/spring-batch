898e1eb38e01d13f62a5b91b509ac35b0c137899
rohitkain729 <rohit729verma@gmail.com>
2025-10-20 14:08:54 +0530

    first commit code update using method chanining

--- 57-springboot-csv-db-method-chaning/src/main/java/com/app/config/BatchConfig.java
+++ 57-springboot-csv-db-method-chaning/src/main/java/com/app/config/BatchConfig.java
@@ -1,26 +1,25 @@
 package com.app.config;
 
-import com.app.listener.JobMonitoringListener;
-import com.app.model.Employee;
-import com.app.processor.EmployeeItemProcessor;
-import lombok.Builder;
+import javax.sql.DataSource;
+
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
-import org.springframework.batch.item.database.BeanPropertyItemSqlParameterSourceProvider;
 import org.springframework.batch.item.database.JdbcBatchItemWriter;
+import org.springframework.batch.item.database.builder.JdbcBatchItemWriterBuilder;
 import org.springframework.batch.item.file.FlatFileItemReader;
-import org.springframework.batch.item.file.mapping.BeanWrapperFieldSetMapper;
-import org.springframework.batch.item.file.mapping.DefaultLineMapper;
-import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
+import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
-import javax.sql.DataSource;
+
+import com.app.listener.JobMonitoringListener;
+import com.app.model.Employee;
+import com.app.processor.EmployeeItemProcessor;
 
 @Configuration
 @EnableBatchProcessing
@@ -36,46 +35,61 @@ public class BatchConfig {
 	@Autowired
 	private DataSource ds;
 
-	@Bean(name = "reader") // using anonymus sub class + instance block
+	@Bean(name = "reader")
 	public FlatFileItemReader<Employee> createReader() {
-		FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>() {
-			{
 
-				// call all non static methods
-
-				setResource(new ClassPathResource("Employee_info.csv"));
-				setLineMapper(new DefaultLineMapper<>() {
-					{
-						setLineTokenizer(new DelimitedLineTokenizer() {
-							{
-								setDelimiter(",");
-								setNames("empno", "empname", "empaddrs", "salary");
-							}
-						});
-						setFieldSetMapper(new BeanWrapperFieldSetMapper<Employee>() {
-							{
-								setTargetType(Employee.class);
-							}
-						});
-					}
-				});
-
-			}
-		};
-		return reader;
+		return new FlatFileItemReaderBuilder<Employee>().name("file-reader")
+				.resource(new ClassPathResource("Employee_info.csv")).delimited()
+				.names("empno", "empname", "empaddrs", "salary").targetType(Employee.class).build();
 	}
+	// bidefault it take delimited as ,
 
 	@Bean(name = "writer")
 	public JdbcBatchItemWriter<Employee> createWriter() {
-		JdbcBatchItemWriter<Employee> writer = new JdbcBatchItemWriter<Employee>() {
-			{
-				setDataSource(ds);
-				setSql("INSERT INTO BATCH_EMPLOYEE VALUES (:empno,:empname,:empaddrs,:salary,:grossSalary,:netSalary)");
-				setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Employee>());
-			}
-		};
-		return writer;
+
+		return new JdbcBatchItemWriterBuilder<Employee>().dataSource(ds)
+				.sql("INSERT INTO BATCH_EMPLOYEE VALUES (:empno,:empname,:empaddrs,:salary,:grossSalary,:netSalary)")
+				.beanMapped().build();
 	}
+
+//	@Bean(name = "reader") // using anonymus sub class + instance block
+//	public FlatFileItemReader<Employee> createReader() {
+//		FlatFileItemReader<Employee> reader = new FlatFileItemReader<Employee>() {
+//			{
+//
+//				// call all non static methods
+//
+//				setResource(new ClassPathResource("Employee_info.csv"));
+//				setLineMapper(new DefaultLineMapper<>() {
+//					{
+//						setLineTokenizer(new DelimitedLineTokenizer() {
+//							{
+//								setDelimiter(",");
+//								setNames("empno", "empname", "empaddrs", "salary");
+//							}
+//						});
+//						setFieldSetMapper(new BeanWrapperFieldSetMapper<Employee>() {
+//							{
+//								setTargetType(Employee.class);
+//							}
+//						});
+//					}
+//				});
+//
+//			}
+//		};
+//		return reader;
+//	}
+
+//	@Bean(name = "writer")
+//	public JdbcBatchItemWriter<Employee> createWriter(){
+//		JdbcBatchItemWriter<Employee> writer= new JdbcBatchItemWriter<Employee>() {{
+//			setDataSource(ds);
+//		 setSql("INSERT INTO BATCH_EMPLOYEE VALUES (:empno,:empname,:empaddrs,:salary,:grossSalary,:netSalary)");	
+//	setItemSqlParameterSourceProvider(new BeanPropertyItemSqlParameterSourceProvider<Employee>());
+//		}};
+//		return writer;
+//	}
 
 	// reader(old way)
 //    @Bean(name = "reader")
---
+++ 57-springboot-csv-db-method-chaning/src/main/java/com/app/model/Employee.java
@@ -0,0 +1,19 @@
+package com.app.model;
+
+
+import lombok.Data;
+
+@Data
+public class Employee {
+
+    private Integer empno;
+    private String empname;
+    private String empaddrs;
+    private Double salary;
+    private Double grossSalary;
+    private Double netSalary;
+
+
+
+
+}
